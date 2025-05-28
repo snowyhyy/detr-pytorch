b71d1db2da91813a85544941f28066d43cf272e6
snowy <1447048804@qq.com>
2025-05-28 14:33:24 +0800

    detr修改完毕

--- models/detr.py
+++ models/detr.py
@@ -1,3 +1,12 @@
+'''
+Author: snowy
+Email: 1447048804@qq.com
+Date: 2025-05-28 09:47:22
+Version: 0.0.1
+LastEditors: snowy 1447048804@qq.com
+LastEditTime: 2025-05-28 14:32:52
+Description: 根据官方的实现，添加中文注释，源码：https://github.com/facebookresearch/detr.git
+'''
 
 """
 DETR 模型和标准类
@@ -195,12 +204,169 @@ class SetCriterion(nn.Module):
         }
         return losses
     
+    def _get_src_permutation_idx(self, indices):
+        # 获取预测框匹配索引
+        batch_idx = torch.cat([torch.full_like(src, i) for i, (src, _) in enumerate(indices)])
+        src_idx = torch.cat([src for (src, _) in indices])
+        return batch_idx, src_idx
     
+    def _get_tgt_permutation_idx(self, indices):
+        # 获取真实框匹配索引
+        batch_idx = torch.cat([torch.full_like(tgt, i) for i, (_, tgt) in enumerate(indices)])
+        tgt_idx = torch.cat([tgt for (_, tgt) in indices])
+        return batch_idx, tgt_idx
+
+    def get_loss(self, loss, outputs, targets, indices, num_boxes, **kwargs):
+        loss_map = {
+            'labels': self.loss_labels,
+            'cardinality': self.loss_cardinality,
+            'boxes': self.loss_boxes,
+            'masks': self.loss_masks,
+        }
+        assert loss in loss_map, f'do you really want to compute {loss} loss?'
+        return loss_map[loss](outputs, targets, indices, num_boxes, **kwargs)
+    
+    def forward(self, outputs, targets):
+        """
+        损失计算的实现
+        Args:
+            outputs: 张量字典，模型的输出
+            targets: 字典列表，len(targets) == batch_size
+        """
+        outputs_without_aux = {k: v for k, v in outputs.items() if k != 'aux_outputs'} # 去除辅助输出
+
+        # 检索最后一层的输出与目标之间的匹配
+        indices = self.matcher(outputs_without_aux, targets) # 匈牙利算法匹配
+
+        # 计算所有节点上目标框的平均数量，以进行规范化
+        num_boxes = sum(len(t["labels"]) for t in targets)
+        num_boxes = torch.as_tensor([num_boxes], dtype=torch.float, device=next(iter(outputs.values())).device)
+        if is_dist_avail_and_initialized():
+            torch.distributed.all_reduce(num_boxes)
+        num_boxes = torch.clamp(num_boxes / get_world_size(), min=1).item()
+
+        # 计算所有损失
+        losses = {}
+        for loss in self.losses:
+            losses.update(self.get_loss(loss, outputs, targets, indices, num_boxes))
+
+        # 计算辅助损失时，需要在每个中间层的输出上计算损失
+        if 'aux_outputs' in outputs:
+            for i, aux_outputs in enumerate(outputs['aux_outputs']):
+                indices = self.matcher(aux_outputs, targets)
+                for loss in self.losses:
+                    if loss == 'masks':
+                        # 中间层的掩码损失计算成本太高，因此忽略
+                        continue
+                    kwargs = {}
+                    if loss == 'labels':
+                        # 仅对最后一层进行日志记录
+                        kwargs = {'log': False}
+                    l_dict = self.get_loss(loss, aux_outputs, targets, indices, num_boxes, **kwargs)
+                    l_dict = {k + f'_{i}': v for k, v in l_dict.items()}
+                    losses.update(l_dict)
+        
+        return losses
 
 
+class PostProcess(nn.Module):
+    """
+    此模块将模型的输出转换为cocoapi所期望的格式
+    """
+    @torch.no_grad()
+    def forward(self, outputs, target_sizes):
+        """
+        Args:
+            outputs: 模型的原始输出
+            target_sizes: 图像的目标尺寸，形状为 [batch_size, 2]
+                对于评估，这必须是原始图像大小
+                对于可视化，这是数据增强后但填充前的图像尺寸
+        """
+        out_logits, out_bbox = outputs['pred_logits'], outputs['pred_boxes']
+
+        assert len(out_logits) == len(target_sizes)
+        assert target_sizes.shape[1] == 2
+
+        prob = F.softmax(out_logits, -1)
+        scores, labels = prob[..., :-1].max(-1) # 排除最后一个维度（背景类）
+
+        # 转换到 [x0, y0, x1, y1] 格式
+        boxes = box_ops.box_cxcywh_to_xyxy(out_bbox) # [cx, cy, w, h] -> [x0, y0, x1, y1]
+        # 从相对坐标[0, 1]转换到[0, height]绝对坐标
+        img_h, img_w = target_sizes.unbind(1) # 取出图像高度和宽度
+        scale_fct = torch.stack([img_w, img_h, img_w, img_h], dim=1)
+        boxes = boxes * scale_fct[:, None, :]
+
+        results = [{'scores': s, 'labels': l, 'boxes': b} for s, l, b in zip(scores, labels, boxes)]
+
+        return results
 
 
+class MLP(nn.Module):
+    """
+    多层感知机, 也就是 FFN
+    """
+
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers):
+        super().__init__()
+        self.num_layers = num_layers
+        h = [hidden_dim] * (num_layers - 1)
+        self.layers = nn.ModuleList(nn.Linear(n, k) for n, k in zip([input_dim] + h, h + [output_dim]))
+
+    def forward(self, x):
+        for i, layer in enumerate(self.layers):
+            x = F.relu(layer(x)) if i < self.num_layers - 1 else layer(x)
+        return x
 
 
+def build(args):
+    # 这里的 num_classes 指的是 目标最大类别 + 1（背景类）
+    # 比如在 coco 数据集中，目标检测类别数是 80 类，其中最大的类别是 90，所以 num_classes = 91
+    # 在全景分割中，类别包含 物体+背景，为了预留足够的空间，num_classes = 250
+    num_classes = 20 if args.dataset_file != 'coco' else 91
+    if args.dataset_file == "coco_panoptic":
+        # 对于全景分割，只需要添加一个足够大 num_classes, 去容纳 max_obj_id + 1 个类别, 不需要确切的类别数
+        num_classes = 250
+    device = torch.device(args.device)
 
+    backbone = build_backbone(args)
+
+    transformer = build_transformer(args)
+
+    model = DETR(
+        backbone,
+        transformer,
+        num_classes=num_classes,
+        num_queries=args.num_queries,
+        aux_loss=args.aux_loss,
+    )
+    if args.masks:
+        model = DETRsegm(model, freeze_detr=(args.frozen_weights is not None))
+    matcher = build_matcher(args)
+    weight_dict = {'loss_ce': 1, 'loss_bbox': args.bbox_loss_coef}
+    weight_dict['loss_giou'] = args.giou_loss_coef
+    if args.masks:
+        weight_dict["loss_mask"] = args.mask_loss_coef
+        weight_dict["loss_dice"] = args.dice_loss_coef
+    # TODO this is a hack
+    if args.aux_loss:
+        aux_weight_dict = {}
+        for i in range(args.dec_layers - 1):
+            aux_weight_dict.update({k + f'_{i}': v for k, v in weight_dict.items()})
+        weight_dict.update(aux_weight_dict)
+    
+    losses = ['labels', 'boxes', 'cardinality']
+    if args.masks:
+        losses += ["masks"]
+    criterion = SetCriterion(num_classes, matcher=matcher, weight_dict=weight_dict,
+                             eos_coef=args.eos_coef, losses=losses)
+    criterion.to(device)
+    postprocessors = {'bbox': PostProcess()}
+    if args.masks:
+        postprocessors['segm'] = PostProcessSegm()
+        if args.dataset_file == "coco_panoptic":
+            is_thing_map = {i: i <= 90 for i in range(201)}
+            postprocessors["panoptic"] = PostProcessPanoptic(is_thing_map, threshold=0.85)
+
+    return model, criterion, postprocessors
 
